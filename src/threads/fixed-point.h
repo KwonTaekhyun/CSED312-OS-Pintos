7aaeb85e9f5bde05baa60d33c78bf2affb1cff2f
KwonTaekhyun <kth991118@gmail.com>
2021-10-08 09:04:39 +0900

    fixed-point 에러 수정2

--- src/threads/fixed-point.h
+++ src/threads/fixed-point.h
@@ -1,18 +1,7 @@
+#include <stdint.h>
+
 static int Correction = 16384; // 1 << 14
-
 typedef int FP;
-
-FP int_to_FP(int x);
-int FP_to_int(FP x);
-FP FP_to_round(FP x);
-FP FP_add(FP x, FP y);
-FP FP_add_int(FP x, int y);
-FP FP_sub(FP x, FP y);
-FP FP_sub_int(FP x, int y);
-FP FP_mult(FP x, FP y);
-FP FP_mult_int(FP x, int y);
-FP FP_div(FP x, FP y);
-FP FP_div_int(FP x, int y);
 
 FP int_to_FP(int x){
     return x * Correction;
