73f678604dfc99956d3be95029dde38a6a23bbd4
KwonTaekhyun <kth991118@gmail.com>
2021-10-08 09:19:48 +0900

    fixed-point 에러 수정4

--- src/threads/fixed-point.h
+++ src/threads/fixed-point.h
@@ -1,15 +1,50 @@
 #include <stdint.h>
 
 typedef int FP;
+static int Correction = 16384; // 1 << 14
 
-FP int_to_FP(int x);
-int FP_to_int(FP x);
-FP FP_to_round(FP x);
-FP FP_add(FP x, FP y);
-FP FP_add_int(FP x, int y);
-FP FP_sub(FP x, FP y);
-FP FP_sub_int(FP x, int y);
-FP FP_mult(FP x, FP y);
-FP FP_mult_int(FP x, int y);
-FP FP_div(FP x, FP y);
-FP FP_div_int(FP x, int y);
+extern inline FP int_to_FP(int x){
+    return x * Correction;
+}
+
+extern inline FP FP_to_zero(FP x){
+    return x / Correction;
+}
+
+extern inline FP FP_to_round(FP x){
+    if(x >= 0) 
+        return (x + (Correction / 2)) / Correction;
+    return (x - (Correction / 2)) / Correction;
+}
+
+extern inline FP FP_add(FP x, FP y){
+    return x + y;
+}
+
+extern inline FP FP_add_int(FP x, int y){
+    return x + y * Correction;
+}
+
+extern inline FP FP_sub(FP x, FP y){
+    x - y;
+}
+
+extern inline FP FP_sub_int(FP x, int y){
+    x - y * Correction;
+}
+
+extern inline FP FP_mult(FP x, FP y){
+    return (((int64_t) x) * y) / Correction;
+}
+
+extern inline FP FP_mult_int(FP x, int y){
+    return x * y;
+}
+
+extern inline FP FP_div(FP x, FP y){
+    return (((int64_t) x) * Correction) / y;
+}
+
+extern inline FP FP_div_int(FP x, int y){
+    return x / y;
+}
