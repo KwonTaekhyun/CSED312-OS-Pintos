ecd6b82e155fd2d23c6b369b32c022d69f9c7f73
KwonTaekhyun <kth991118@gmail.com>
2021-10-08 09:09:33 +0900

    fixed-point 에러 수정3

---
+++ src/threads/fixed-point.c
@@ -0,0 +1,49 @@
+#include "threads/fixed-point.h"
+
+static int Correction = 16384; // 1 << 14
+
+FP int_to_FP(int x){
+    return x * Correction;
+}
+
+FP FP_to_zero(FP x){
+    return x / Correction;
+}
+
+FP FP_to_round(FP x){
+    if(x >= 0) 
+        return (x + (Correction / 2)) / Correction;
+    return (x - (Correction / 2)) / Correction;
+}
+
+FP FP_add(FP x, FP y){
+    return x + y;
+}
+
+FP FP_add_int(FP x, int y){
+    return x + y * Correction;
+}
+
+FP FP_sub(FP x, FP y){
+    x - y;
+}
+
+FP FP_sub_int(FP x, int y){
+    x - y * Correction;
+}
+
+FP FP_mult(FP x, FP y){
+    return (((int64_t) x) * y) / Correction;
+}
+
+FP FP_mult_int(FP x, int y){
+    return x * y;
+}
+
+FP FP_div(FP x, FP y){
+    return (((int64_t) x) * Correction) / y;
+}
+
+FP FP_div_int(FP x, int y){
+    return x / y;
+}
--- src/threads/fixed-point.h
+++ src/threads/fixed-point.h
@@ -1,50 +1,15 @@
 #include <stdint.h>
 
-static int Correction = 16384; // 1 << 14
 typedef int FP;
 
-FP int_to_FP(int x){
-    return x * Correction;
-}
-
-FP FP_to_zero(FP x){
-    return x / Correction;
-}
-
-FP FP_to_round(FP x){
-    if(x >= 0) 
-        return (x + (Correction / 2)) / Correction;
-    return (x - (Correction / 2)) / Correction;
-}
-
-FP FP_add(FP x, FP y){
-    return x + y;
-}
-
-FP FP_add_int(FP x, int y){
-    return x + y * Correction;
-}
-
-FP FP_sub(FP x, FP y){
-    x - y;
-}
-
-FP FP_sub_int(FP x, int y){
-    x - y * Correction;
-}
-
-FP FP_mult(FP x, FP y){
-    return (((int64_t) x) * y) / Correction;
-}
-
-FP FP_mult_int(FP x, int y){
-    return x * y;
-}
-
-FP FP_div(FP x, FP y){
-    return (((int64_t) x) * Correction) / y;
-}
-
-FP FP_div_int(FP x, int y){
-    return x / y;
-}
+FP int_to_FP(int x);
+int FP_to_int(FP x);
+FP FP_to_round(FP x);
+FP FP_add(FP x, FP y);
+FP FP_add_int(FP x, int y);
+FP FP_sub(FP x, FP y);
+FP FP_sub_int(FP x, int y);
+FP FP_mult(FP x, FP y);
+FP FP_mult_int(FP x, int y);
+FP FP_div(FP x, FP y);
+FP FP_div_int(FP x, int y);
