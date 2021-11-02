dbc1a79506f69201918c67c59fd7da31b549a489
wkcho99 <wkcho@postech.ac.kr>
2021-11-03 01:45:19 +0900

    [p2-2] printf 형식 수정

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -43,7 +43,7 @@ process_execute (const char *file_name)
   strlcpy (arg_copy, fn_copy, PGSIZE);
   char *argv;
   arg_copy = strtok_r(arg_copy," ",&argv);
-  printf("%d\n",arg_copy);
+  printf("%s\n",arg_copy);
   /* Create a new thread to execute FILE_NAME. */
   tid = thread_create (arg_copy, PRI_DEFAULT, start_process, fn_copy);
   if (tid == TID_ERROR)
@@ -67,17 +67,18 @@ start_process (void *file_name_)
   char *token;
   char *save_ptr;
   token = strtok_r(file_name, " ", &save_ptr);
+  printf("token: %s",token);
   argv[0] = token;
   argc = 1;
   while(token!=NULL)
   {
     token = strtok_r(NULL, " ", &save_ptr);
     argv[argc] = token;
-    printf("%s\n", &argv[argc]);
+    printf("%s\n", argv[argc]);
     argc++;
   }
   argc--;
-  printf("%d\n", &argc);
+  printf("%d\n", argc);
   /* Initialize interrupt frame and load executable. */
   memset (&if_, 0, sizeof if_);
   if_.gs = if_.fs = if_.es = if_.ds = if_.ss = SEL_UDSEG;
