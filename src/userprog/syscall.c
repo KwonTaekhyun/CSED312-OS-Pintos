8d7703b66b210b1fb9e690dc7f73b8c1a82db293
KwonTaekhyun <kth991118@gmail.com>
2021-11-07 12:29:39 +0900

    write 전 open에서 denying write

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -157,6 +157,11 @@ int sys_open(char *file_name){
   else{
     fd->index = (list_entry(list_back(fd_list_ptr), struct file_descriptor, elem)->index) + 1;
   }
+
+  if(strcmp(thread_current()->name, file_name) == 0){
+    file_deny_write(file_ptr);
+  }
+
   list_push_back(fd_list_ptr, &fd->elem);
 
   return fd->index;
