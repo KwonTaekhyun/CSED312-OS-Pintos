17b860ef46595322686fb52e76e78e4de6aaa751
KwonTaekhyun <kth991118@gmail.com>
2021-11-07 13:33:29 +0900

    struct type 수정

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -189,7 +189,7 @@ process_exit (void)
   struct list *fd_list = &cur->file_descriptor_list;
   while (!list_empty(fd_list)) {
     struct list_elem *e = list_pop_front (fd_list);
-    struct file_descriptor *fd = list_entry(e, struct file_desc, elem);
+    struct file_descriptor *fd = list_entry(e, struct file_descriptor, elem);
     file_close(fd->file_pt);
   }
   
