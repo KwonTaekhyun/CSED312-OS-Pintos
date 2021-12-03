cd4c0f754a641b573657e2518792e9155d76868a
KwonTaekhyun <kth991118@gmail.com>
2021-12-03 11:41:33 +0900

    [test] 테스트 구문 제거

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -418,7 +418,7 @@ void sys_munmap(int mapid){
       continue;
     }
     // P3-5. File memory mapping
-    printf("Is Dirty?: %s\n", pagedir_is_dirty (current_thread->pagedir, page->vaddr) ? "T" : "F");
+    // printf("Is Dirty?: %s\n", pagedir_is_dirty (current_thread->pagedir, page->vaddr) ? "T" : "F");
     if(pagedir_is_dirty (current_thread->pagedir, page->vaddr)){
         // P3-5. File memory mapping
         // printf("There are something to write (dirty)\n");
