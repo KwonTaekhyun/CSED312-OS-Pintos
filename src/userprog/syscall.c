23ef7a10b3cddb84a0a0c292e99b57034f690f52
KwonTaekhyun <kth991118@gmail.com>
2021-12-03 13:51:27 +0900

    [test] mmap 테스트

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -337,7 +337,7 @@ mapid_t sys_mmap(int fd_idx, void *addr){
   int file_page_num = file_bytes / PGSIZE;
 
     // P3-5-test
-    // printf("file_bytes: %d, file_page_num: %d\n", file_bytes, file_page_num);
+    printf("file_bytes: %d, file_page_num: %d\n", file_bytes, file_page_num);
 
   for(i = 0; i < file_page_num; i++){
     // ** 페이지 단위로 pte 생성 (pte_create_with_file) **
@@ -354,7 +354,7 @@ mapid_t sys_mmap(int fd_idx, void *addr){
     }
 
     // P3-5-test
-    // printf("pte created success?: %d\n", pte_created);
+    printf("pte created success?: %d, %dbytes\n", pte_created, read_bytes);
 
     offset += read_bytes;
   }
@@ -375,7 +375,7 @@ mapid_t sys_mmap(int fd_idx, void *addr){
     file_page_num++;
 
     // P3-5-test
-    // printf("pte created success?: %d\n", pte_created);
+    printf("pte created success?: %d, %dbytes\n", pte_created, read_bytes);
   }
 
   struct thread *current_thread = thread_current();
