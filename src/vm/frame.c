079cdab92d3f5c20619f110cfd65364d4d352c91
KwonTaekhyun <kth991118@gmail.com>
2021-12-04 16:01:09 +0900

    [test] 테스트 코드 삭제

--- src/vm/frame.c
+++ src/vm/frame.c
@@ -22,7 +22,7 @@ struct frame *frame_allocate(enum palloc_flags flags, struct pte *pte)
         // printf("before) frame table size: %d\n", list_size(&frame_table));
         frame = frame_evict(flags);
         // printf("after) frame table size: %d\n", list_size(&frame_table));
-        printf("frame address: %p\n", frame->addr);
+        // printf("frame address: %p\n", frame->addr);
     }
     else{
         frame = malloc(sizeof(struct frame));
