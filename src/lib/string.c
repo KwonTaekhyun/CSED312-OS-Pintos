6190b9ed3c709ecd949b40f11241f39d5ac61863
KwonTaekhyun <kth991118@gmail.com>
2021-12-04 11:24:25 +0900

    [test] 테스트 구문 추가

--- src/lib/string.c
+++ src/lib/string.c
@@ -284,7 +284,7 @@ memset (void *dst_, int value, size_t size)
   
   while (size-- > 0){
     *dst++ = value;
-    printf("address: %p", dst);
+    printf("address: %p\n", dst);
   }
 
   return dst_;
