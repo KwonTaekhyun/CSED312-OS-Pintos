2d34d525a0a98400fc4e448e5db90d63fc185f23
KwonTaekhyun <kth991118@gmail.com>
2021-12-04 15:34:20 +0900

    [test] 테스트 코드 추가

--- src/vm/frame.c
+++ src/vm/frame.c
@@ -19,9 +19,10 @@ struct frame *frame_allocate(enum palloc_flags flags, struct pte *pte)
     // palloc_get_page()를 통해 페이지 할당
     void *page = palloc_get_page(flags);
     if(!page){
-        printf("before) frame table size: %d\n", list_size(&frame_table));
+        // printf("before) frame table size: %d\n", list_size(&frame_table));
         frame = frame_evict(flags);
-        printf("after) frame table size: %d\n", list_size(&frame_table));
+        // printf("after) frame table size: %d\n", list_size(&frame_table));
+        printf("frame address: %p\n", frame->addr);
     }
     else{
         frame = malloc(sizeof(struct frame));
