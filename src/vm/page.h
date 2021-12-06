e037f7220f187044f6db52d9609f6283759e4689
wkcho99 <wkcho@postech.ac.kr>
2021-12-07 02:35:29 +0900

    [p3] load_file 형식 수정

--- src/vm/page.h
+++ src/vm/page.h
@@ -38,7 +38,7 @@ bool pte_delete(struct hash *pt, struct pte *pte);
 struct pte *pte_find(void *vaddr);
 void pt_destroy(struct hash *pt);
 void pt_destroy_func(struct hash_elem *e, void *aux);
-bool load_file(struct frame *frame, struct pte *p);
+bool load_file(void *addr, struct pte *p);
 
 /* P3-5. File memory mapping */
 void pt_destory_by_addr (void* addr);
