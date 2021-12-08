e018dbe2ec1e9a4faaaaf6a2e7c651de17299846
wkcho99 <wkcho@postech.ac.kr>
2021-12-08 23:35:25 +0900

    오류 수정

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -346,8 +346,8 @@ load (const char *file_name, void (**eip) (void), void **esp)
   if (t->pagedir == NULL) 
     goto done;
   process_activate ();
-  &t->page_table = malloc(sizeof (struct hash));
-  if(t->page_table == NULL) goto done;
+  //t->page_table = malloc(sizeof (struct hash));
+  //if(t->page_table == NULL) goto done;
   pt_init(&t->page_table);
   /* Open executable file. */
   // final-test
